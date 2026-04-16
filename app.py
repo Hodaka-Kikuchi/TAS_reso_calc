6ad599f21c517f64270b752c2784b264b971e789
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-04-16 16:51:14 -0400

    revise

--- app.py
+++ app.py
@@ -180,35 +180,56 @@ with st.container(border=True):
                 l3 = st.number_input("l3", value=0)
 
 with st.container(border=True):
-    st.subheader("Collimator conditions")
+    st.subheader("Collimator conditions (unit:min)")
 
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
         with st.container(border=True):
-            st.markdown("### 1st collimator")
-            col1_h = st.number_input("1st H (min)", value=80, key="col1_h")
-            col1_v = st.number_input("1st V (min)", value=240, key="col1_v")
+            st.markdown("### 1st")
+            col1_h = st.number_input("horizontal", value=80, key="col1_h")
+            col1_v = st.number_input("vertical", value=240, key="col1_v")
 
     with col2:
         with st.container(border=True):
-            st.markdown("### 2nd collimator")
-            col2_h = st.number_input("2nd H (min)", value=80, key="col2_h")
-            col2_v = st.number_input("2nd V (min)", value=240, key="col2_v")
+            st.markdown("### 2nd")
+            col2_h = st.number_input("horizontal", value=80, key="col2_h")
+            col2_v = st.number_input("vertical", value=240, key="col2_v")
 
     with col3:
         with st.container(border=True):
-            st.markdown("### 3rd collimator")
-            col3_h = st.number_input("3rd H (min)", value=80, key="col3_h")
-            col3_v = st.number_input("3rd V (min)", value=240, key="col3_v")
+            st.markdown("### 3rd")
+            col3_h = st.number_input("horizontal", value=80, key="col3_h")
+            col3_v = st.number_input("vertical", value=240, key="col3_v")
 
     with col4:
         with st.container(border=True):
-            st.markdown("### 4th collimator")
+            st.markdown("### 4th")
             col4_h = st.number_input("4th H (min)", value=80, key="col4_h")
             col4_v = st.number_input("4th V (min)", value=240, key="col4_v")
 
+with st.container(border=True):
+    st.subheader("Mosacis conditions (unit:min)")
 
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        with st.container(border=True):
+            st.markdown("### monochromator")
+            mono_mos_h= st.number_input("horizontal", value=80, key="mono_mos_h")
+            mono_mos_v = st.number_input("vertical", value=240, key="mono_mos_v")
+
+    with col2:
+        with st.container(border=True):
+            st.markdown("### sample")
+            sam_mos_h = st.number_input("horizontal", value=80, key="sam_mos_h")
+            sam_mos_v = st.number_input("vertical", value=240, key="sam_mos_v")
+
+    with col3:
+        with st.container(border=True):
+            st.markdown("### analyzer")
+            ana_mos_h = st.number_input("horizontal", value=80, key="ana_mos_h")
+            ana_mos_v = st.number_input("vertical", value=240, key="ana_mos_v")
 ##################################################################################3
 
 if st.button("Calc"):
