17d2ecb67acd76059e19d510c8a55dd0b690b955
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-04-16 16:19:47 -0400

    revise

--- app.py
+++ app.py
@@ -124,6 +124,7 @@ def UB_calc(sv1, sv2, astar, bstar, cstar,
 # =========================
 # Streamlit UI
 # =========================
+st.title("TAS Resolution Calculator")
 st.set_page_config(page_title="TAS Resolution Calculator", layout="wide")
 
 st.subheader("Lattice parameters")
