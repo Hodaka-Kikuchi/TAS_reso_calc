00098971d8c509d5f871d5e39310e89e065bc65a
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-04-20 15:11:17 -0400

    revise

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ with st.container(border=True):
 with st.container(border=True):
     st.subheader("Instrument setting")
 
-    col1, col2, col3 = st.columns([3, 1, 4])
+    col1, col2, col3 = st.columns([3, 2, 4])
     with col1:
         with st.container(border=True):
             st.markdown("<h5>Scan configuration</h5>", unsafe_allow_html=True)
