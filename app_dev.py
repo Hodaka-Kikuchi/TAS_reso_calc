3cfececf143f5e397eef84c68d2410bf6587cdad
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-20 19:03:47 -0400

    revise

--- app_dev.py
+++ app_dev.py
@@ -653,7 +653,7 @@ with st.container(border=True):
 
             results = st.session_state.scan_results
 
-            col1, col2, col3 = st.columns([4, 1, 1])
+            col1, col2, col3 = st.columns(3)
 
             with col1:
                 i = st.slider(
