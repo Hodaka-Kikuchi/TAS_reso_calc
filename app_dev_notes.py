1f6fbe777814c1fea4bfe34a028698fea6767db7
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-20 17:11:19 -0400

    update

--- app_dev_notes.py
+++ app_dev_notes.py
@@ -2,8 +2,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title("Development Notes")
 
-lang = st.radio("Language", ["JP", "EN"], horizontal=True)
-
 with open("dev_notes.txt", "r", encoding="utf-8") as f:
     lines = [line.strip() for line in f if line.strip()]
 
