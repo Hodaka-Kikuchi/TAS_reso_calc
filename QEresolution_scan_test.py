90c1d03316fbe32bf571275ee716f3e58cebfd0d
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-20 11:14:07 -0400

    update

--- QEresolution_scan_test.py
+++ QEresolution_scan_test.py
@@ -847,4 +847,8 @@ def calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,fo
     ax4.grid(True)
 
     #return A_sets,QE_sets,RM, fig
-    return A
+    # matrix確認
+    # 問題なしA,
+    # 問題あり
+
+    return B
