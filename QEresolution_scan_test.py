fcc25fd79cd69100108f2dce3aa95786f7f1953c
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-20 11:19:23 -0400

    update

--- QEresolution_scan_test.py
+++ QEresolution_scan_test.py
@@ -135,7 +135,8 @@ def calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,fo
     X_vals, Y_vals, Z_vals, W_vals = [], [], [], []
     
     for index in [0, -1]:
-        A1, -A2, A3 = A_sets[index]
+        A1, A2, A3 = A_sets[index]
+        A2 = -A2
         hw = QE_sets[index][0]
         
         if energy_mode == "Ei fixed":
