ca92caf154e85e05b5667a87dcd4083c7f5df257
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-20 19:02:43 -0400

    revise

--- app_dev.py
+++ app_dev.py
@@ -653,17 +653,9 @@ with st.container(border=True):
 
             results = st.session_state.scan_results
 
-            col1, col2, col3 = st.columns(3)
+            col1, col2, col3 = st.columns([1, 1, 4])
 
             with col1:
-                if st.button("◀ Prev"):
-                    st.session_state.scan_slider = max(1, st.session_state.scan_slider - 1)
-
-            with col2:
-                if st.button("Next ▶"):
-                    st.session_state.scan_slider = min(len(results), st.session_state.scan_slider + 1)
-
-            with col3:
                 i = st.slider(
                     "Scan index",
                     1,
@@ -671,6 +663,14 @@ with st.container(border=True):
                     1,
                     key="scan_slider"
                 )
+
+            with col2:
+                if st.button("◀ Prev"):
+                    st.session_state.scan_slider = max(1, st.session_state.scan_slider - 1)
+
+            with col3:
+                if st.button("Next ▶"):
+                    st.session_state.scan_slider = min(len(results), st.session_state.scan_slider + 1)
 
             RM, fig = results[i - 1]
 
