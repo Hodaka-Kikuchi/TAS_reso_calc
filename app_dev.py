a25708e47c5e03ce66acbd04e5875260f7a231bb
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-20 16:49:23 -0400

    update

--- app_dev.py
+++ app_dev.py
@@ -559,9 +559,9 @@ with st.container(border=True):
 
         if st.button("Calc single"):
             rl = RL_calc(lc_param)
-            mat1 = calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,focusing,geom,calc_params)
+            mat1,mat2 = calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,focusing,geom,calc_params)
             st.text(np.array2string(mat1, precision=18, suppress_small=False))
-            #st.text(np.array2string(mat2, precision=18, suppress_small=False))
+            st.text(np.array2string(mat2, precision=18, suppress_small=False))
             #st.text(np.array2string(mat3, precision=18, suppress_small=False))
             #RM, fig = calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,focusing,geom,calc_params)
             #st.write("A_sets", A_sets)
