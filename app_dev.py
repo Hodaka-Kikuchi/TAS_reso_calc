ec780a81886d5f1ce90695db9c49db15036432fd
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-21 11:22:28 -0400

    test

--- app_dev.py
+++ app_dev.py
@@ -14,6 +14,8 @@ from QEresolution_scan_dev import calcresolution_scan3 # スライダー形式�
 
 # デフォルト数値を読み込み
 from instrument_defaults import CTAX
+from instrument_defaults import MANUAL
+
 
 # 使用方法
 # powershellで　cd C:\Users\h34\Documents\Python\TAS_reso_calc_web
@@ -48,7 +50,7 @@ def load_instrument_defaults(instrument):
     if instrument == "CTAX":
         return CTAX
     elif instrument == "Manual":
-        return CTAX
+        return MANUAL
     else:
         return {}
     
--- instrument_defaults.py
+++ instrument_defaults.py
@@ -68,7 +68,7 @@ CTAX = {
     },
 }
 
-Manual = {
+MANUAL = {
     "supermirror": {
         "enabled": False,
         "m_value": 1.0
