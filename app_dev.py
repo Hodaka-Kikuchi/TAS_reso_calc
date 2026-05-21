95625c39fd75a0366e056605a2b53f3dcdd5fdfb
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-21 09:44:21 -0400

    value change

--- app_dev.py
+++ app_dev.py
@@ -385,7 +385,7 @@ with st.container(border=True):
                 c3, c4 = st.columns(2)
 
                 with c3:
-                    fc_mono_v = st.checkbox("Vertical", key="fc_mono_v")
+                    fc_mono_v = st.checkbox("Vertical", value=True, key="fc_mono_v")
                 with c4:
                     mono_v_blade = st.number_input(
                         "Blade number",
@@ -413,7 +413,7 @@ with st.container(border=True):
                 c3, c4 = st.columns(2)
 
                 with c3:
-                    fc_ana_v = st.checkbox("Vertical", key="fc_ana_v")
+                    fc_ana_v = st.checkbox("Vertical", value=True, key="fc_ana_v")
                 with c4:
                     ana_v_blade = st.number_input(
                         "Blade number",
