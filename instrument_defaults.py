cbcb4073bcdbf05f579ea78c7594eed12f714ace
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-21 11:47:19 -0400

    test

--- instrument_defaults.py
+++ instrument_defaults.py
@@ -1,73 +1,5 @@
 # instrument_defaults.py
 
-CTAX = {
-    "supermirror": {
-        "enabled": True,
-        "m_value": 1.2
-    },
-
-    "collimator": {
-        "1st_h": 80,
-        "1st_v": 240,
-        "2nd_h": 120,
-        "2nd_v": 240,
-        "3rd_h": 80,
-        "3rd_v": 240,
-        "4th_h": 120,
-        "4th_v": 240,
-    },
-
-    "monochromator": {
-        "crystal": "PG002",
-        "mosaic_h": 30,
-        "mosaic_v": 30,
-        "width": 0.07,
-        "height": 0.02,
-        "thickness": 0.002,
-        "vfocus": True,
-        "hfocus": False,
-        "blade_h": 1,
-        "blade_v": 7
-    },
-
-    "analyzer": {
-        "crystal": "PG002",
-        "mosaic_h": 30,
-        "mosaic_v": 30,
-        "width": 0.022,
-        "height": 0.022,
-        "thickness": 0.002,
-        "vfocus": True,
-        "hfocus": False,
-        "blade_h": 9,
-        "blade_v": 7
-    },
-
-    "detector": {
-        "width": 0.032,
-        "height": 0.120
-    },
-
-    "configuration": {
-        "energy_mode": "Ef fixed",
-        "Ef": 4.8,
-        "geometry": "W",
-        "sign": "-+-"
-    },
-
-    "distance": {
-        "L0": 53,
-        "L1": 1.6,
-        "L2": 1.06,
-        "L3": 0.5
-    },
-
-    "beam": {
-        "width": 0.07,
-        "height": 0.14
-    },
-}
-
 MANUAL = {
     "supermirror": {
         "enabled": False,
@@ -133,6 +65,74 @@ MANUAL = {
     "beam": {
         "width": 0.08,
         "height": 0.15
+    },
+}
+
+CTAX = {
+    "supermirror": {
+        "enabled": True,
+        "m_value": 1.2
+    },
+
+    "collimator": {
+        "1st_h": 80,
+        "1st_v": 240,
+        "2nd_h": 120,
+        "2nd_v": 240,
+        "3rd_h": 80,
+        "3rd_v": 240,
+        "4th_h": 120,
+        "4th_v": 240,
+    },
+
+    "monochromator": {
+        "crystal": "PG002",
+        "mosaic_h": 30,
+        "mosaic_v": 30,
+        "width": 0.07,
+        "height": 0.02,
+        "thickness": 0.002,
+        "vfocus": True,
+        "hfocus": False,
+        "blade_h": 1,
+        "blade_v": 7
+    },
+
+    "analyzer": {
+        "crystal": "PG002",
+        "mosaic_h": 30,
+        "mosaic_v": 30,
+        "width": 0.022,
+        "height": 0.022,
+        "thickness": 0.002,
+        "vfocus": True,
+        "hfocus": False,
+        "blade_h": 9,
+        "blade_v": 7
+    },
+
+    "detector": {
+        "width": 0.032,
+        "height": 0.120
+    },
+
+    "configuration": {
+        "energy_mode": "Ef fixed",
+        "Ef": 4.8,
+        "geometry": "W",
+        "sign": "-+-"
+    },
+
+    "distance": {
+        "L0": 53,
+        "L1": 1.6,
+        "L2": 1.06,
+        "L3": 0.5
+    },
+
+    "beam": {
+        "width": 0.07,
+        "height": 0.14
     },
 }
 
