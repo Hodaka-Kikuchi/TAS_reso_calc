04e4f9f73115222436f853e3bf1bc3f251cb4786
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-21 12:00:45 -0400

    test

--- app_dev.py
+++ app_dev.py
@@ -14,7 +14,7 @@ from QEresolution_scan_dev import calcresolution_scan3 # スライダー形式�
 
 # デフォルト数値を読み込み
 from defaults_ctax import CTAX
-from defaults_manul import Manual
+from defaults_manual import Manual
 
 # 使用方法
 # powershellで　cd C:\Users\h34\Documents\Python\TAS_reso_calc_web
