7b953778bbf3c55ea575bffaffeb26cfbeeb736e
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-22 17:25:36 -0400

    test

--- QEresolution_scan_dev2.py
+++ QEresolution_scan_dev2.py
@@ -550,7 +550,7 @@ def calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,fo
     max_z, coords_z = find_max_along_axis(RM, axis="z")# E
     max_w, coords_w = find_max_along_axis(RM, axis="w")# E
     
-    def nice_limit(x, scale=1.25):
+    def nice_limit(x, scale=1.05):
 
         v = abs(x) * scale
 
