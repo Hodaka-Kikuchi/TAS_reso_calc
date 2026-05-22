1c620ace32c9d7e5d6c455ff5d7675c4aa2b57ba
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-22 17:07:56 -0400

    test

--- QEresolution_scan_dev2.py
+++ QEresolution_scan_dev2.py
@@ -557,16 +557,16 @@ def calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,fo
         # ----------------------------
         # ステップルール
         # ----------------------------
-        if v <= 0.025:
-            step = 0.025
+        if v <= 0.01:
+            step = 0.01
         elif v <= 0.05:
             step = 0.05
         elif v <= 0.1:
             step = 0.1
         elif v <= 0.5:
-            step = 0.1
+            step = 0.5
         elif v <= 1.0:
-            step = 0.2
+            step = 1.0
         else:
             step = 0.5
 
