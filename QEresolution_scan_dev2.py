e84c9ca1d4b33de78a85e7ca9423760a4c2ac98a
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-05-22 17:32:01 -0400

    test

--- QEresolution_scan_dev2.py
+++ QEresolution_scan_dev2.py
@@ -550,25 +550,28 @@ def calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,fo
     max_z, coords_z = find_max_along_axis(RM, axis="z")# E
     max_w, coords_w = find_max_along_axis(RM, axis="w")# E
     
-    def nice_limit(x, scale=1.25):
+    def nice_round(v):
+        if v <= 0:
+            return 0
 
-        v = abs(x) * scale
+        exponent = np.floor(np.log10(v))
+        base = v / 10**exponent
 
-        if v <= 0.05:
-            step = 0.05
-        elif v <= 0.1:
-            step = 0.1
-        elif v <= 0.5:
-            step = 0.1
+        if base <= 1:
+            nice_base = 1
+        elif base <= 2:
+            nice_base = 2
+        elif base <= 5:
+            nice_base = 5
         else:
-            step = 0.5
+            nice_base = 10
 
-        return np.ceil(v / step) * step
+        return nice_base * 10**exponent
 
-    Xrange_lim = nice_limit(max_x)
-    Yrange_lim = nice_limit(max_y)
-    Zrange_lim = nice_limit(max_z)
-    Wrange_lim = nice_limit(max_w)
+    Xrange_lim = nice_round(max_x)
+    Yrange_lim = nice_round(max_y)
+    Zrange_lim = nice_round(max_z)
+    Wrange_lim = nice_round(max_w)
     
     '''
     scale = 1.25
