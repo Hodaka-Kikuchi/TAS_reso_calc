71237ebdbf54e2355bb717c714e0260ae6b7a158
Hodaka-Kikuchi <hodaka.kikuchi@gmail.com>
2026-06-22 14:04:41 +0900

    update

--- QEresolution_scan.py
+++ QEresolution_scan.py
@@ -823,4 +823,4 @@ def calcresolution_scan3(lc_param,rl,col_param,mos_param,config,approximation,fo
     ax4.grid(True)
 
     #return A_sets,QE_sets,RM, fig
-    return RM, fig, A_sets
+    return RM, fig
